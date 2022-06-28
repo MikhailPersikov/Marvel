909efbf3f85739ef09f80d548dd585e8562d26bb
Mikhail Persikov <gt.gluck@gmail.com>
2022-06-28 21:47:51 +0300

    Created hook

--- marvel_starter/test.js
+++ marvel_starter/test.js
@@ -1,25 +1,8 @@
-const str = `Originally a partner of the mind-altering assassin Black Swan, Nijo spied on Deadpool as part of the Swan's plan to exact revenge for Deadpool falsely taking credit for the Swan's assassination of the Four Winds crime family, which included Nijo's brother`
-
-checkLenghtDescription = (desc) => {
-  let index;
-  let counter = 220;
-  if(desc.length <= counter) return desc
-  else {
-    function checkSpaceInDesc(){
-      if(counter > 170) {
-        counter--
-        if(desc[counter] === ' ') index = counter;
-        checkSpaceInDesc();
-      }
-    }
-    checkSpaceInDesc();
-  }
-
-  return desc.slice(0,index) + ' ...';
+const plus = (i) => {
+  return (i++)
 }
 
-function checkLenghtDescription(desc) {
-  
-}
-
-console.log(checkLenghtDescription(str))
+console.log(plus((()=>{ return 2} )()))
+console.log(plus((()=>{ return 2} )()))
+console.log(plus((()=>{ return 2} )()))
+console.log(plus((()=>{ return 2} )()))
