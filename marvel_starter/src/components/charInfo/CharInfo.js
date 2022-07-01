556b09ecaaaf12d84c065a13b50cbdc7c1b4d11f
Mikhail Persikov <gt.gluck@gmail.com>
2022-07-01 19:58:14 +0300

    Changed class CharInfo to hook

--- marvel_starter/src/components/charInfo/CharInfo.js
+++ marvel_starter/src/components/charInfo/CharInfo.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelServices';
@@ -6,113 +6,96 @@ import Skeleton from '../skeleton/Skeleton';
 
 import './charInfo.scss';
 
-class CharInfo extends Component {
+const CharInfo = (props) => {
 
-    state = {
-        char: null,
-        loading: false,
-        error: false,
-    }
-    
-    marvelService = new MarvelService();
+    const [char, setChar] = useState(null);
+    const [loading, setloading] = useState(false);
+    const [error, setError] = useState(false);
 
-    componentDidMount() {
-        this.updateChar();
-    }
+    const marvelService = new MarvelService();
 
-    componentDidUpdate(prevProps){
-        if (this.props.charId !== prevProps.charId) {
-            this.updateChar();
-        }
-    }
+    useEffect(() => {
+        updateChar();
+    }, [props.charId])
 
-    updateChar = () => {
-        const { charId } = this.props;
-        if (!charId) return
+    const updateChar = () => {
+        const { charId } = props;
+        if (!charId) return;
 
-        this.onCharLoading();
+        onCharLoading();
 
-        this.marvelService
+        marvelService
             .getCharacter(charId)
-            .then(this.onCharLoaded)
-            .catch(this.onError)
+            .then(onCharLoaded)
+            .catch(onError)
     }
 
-    onCharLoaded = (char) => {
-        this.setState({ 
-             char,
-             loading: false })
+    const onCharLoaded = (char) => {
+        setChar(char);
+        setloading(false);
     }
 
-    onCharLoading = () => {
-        this.setState({
-            loading:true
-        })
+    const onCharLoading = () => {
+        setloading(false)
     }
 
-    onError = () => {
-        this.setState({
-            loading: false,
-            error: true,
-        })
+    const onError = () => {
+        setloading(false);
+        setError(true);
     }
 
-    render() {
-        const {char, loading, error} = this.state;
+    const skeleton = char || loading || error ? null : <Skeleton />;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error || !char) ? <View char={char} /> : null;
 
-        const skeleton = char || loading || error ? null : <Skeleton/>;
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error || !char) ? <View char={char}/> : null;
-        
-        return (
-            <div className="char__info">
-                {skeleton}
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        )
-    }
+    return (
+        <div className="char__info">
+            {skeleton}
+            {errorMessage}
+            {spinner}
+            {content}
+        </div>
+    )
 }
 
-const View = ({char}) => {
-    const {name,description, thumbnail, homepage, wiki, comics, availableImage} = char;
+const View = ({ char }) => {
+    const { name, description, thumbnail, homepage, wiki, comics, availableImage } = char;
     const className = (availableImage) ? "randomchar__img" : "randomchar__img center"
     return (
         <>
-        <div className="char__basics">
-        <img src={thumbnail} alt={name} className={className}/>
-        <div>
-            <div className="char__info-name">{name}</div>
-            <div className="char__btns">
-                <a href={homepage} className="button button__main">
-                    <div className="inner">Homepage</div>
-                </a>
-                <a href={wiki} className="button button__secondary">
-                    <div className="inner">Wiki</div>
-                </a>
+            <div className="char__basics">
+                <img src={thumbnail} alt={name} className={className} />
+                <div>
+                    <div className="char__info-name">{name}</div>
+                    <div className="char__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">Homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">Wiki</div>
+                        </a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div className="char__descr">
-        {description}
-    </div>
-    <div className="char__comics">Comics:</div>
-    <ul className="char__comics-list">
-        {comics.length > 0 ? null : "There is no comics with this character"}
-        { 
-            comics.map((item,i) => {
-                if (i > 9) return
-                return (
-                    <li key={i} className="char__comics-item">
-                        {item.name}
-                    </li>
-                )
-            })
-        }
-    </ul>
-    </>
+            <div className="char__descr">
+                {description}
+            </div>
+            <div className="char__comics">Comics:</div>
+            <ul className="char__comics-list">
+                {comics.length > 0 ? null : "There is no comics with this character"}
+                {
+                    comics.map((item, i) => {
+                        if (i > 9) return
+                        return (
+                            <li key={i} className="char__comics-item">
+                                {item.name}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </>
     )
 }
 
